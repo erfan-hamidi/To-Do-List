2028190426c966e33844c49cc13e1b22cd15d196
Erfan-Hamidi <erfan9757@gmail.com>
2023-11-23 10:17:38 +0330

    add frontend & view

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 # Create your views here.
+class TodoList(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
