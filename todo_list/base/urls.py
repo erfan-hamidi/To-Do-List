be65dcde99ebb0dfc7d357416a059f99a5f28afe
Erfan-Hamidi <erfan9757@gmail.com>
2023-12-10 19:03:03 +0330

    add jwt

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('task/', views.TodoList.as_view(), name= 'todo_list'),
@@ -7,5 +8,7 @@ urlpatterns = [
     path('task-create/', views.CreateTask.as_view(), name='task-create'),
     path('task-update/<int:pk>',views.UpdateTask.as_view(),name='task-update'),
     path('task-delete/<int:pk>', views.DeleteTask.as_view(), name='task-delete'),
-
+    path('login/', TokenObtainPairView.as_view(),name='login'),
+    path('verify/', TokenVerifyView.as_view(), name='verify'),
+    path('refresh', TokenRefreshView.as_view(), name='refresh')
 ]
